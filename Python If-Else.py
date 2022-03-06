880b630293ed294d5eca3c5e76f957a4480eb039
coder9051 <84278583+coder9051@users.noreply.github.com>
2022-03-06 16:56:41 +0530

    Create Python If-Else.py

---
+++ Python If-Else.py
@@ -0,0 +1,20 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+a = int(input())
+if a%2 == 0:
+    if a>=2 and a<=5:
+        print("Not Weird")
+    elif a>=6 and a<=20:
+        print("Weird")
+    else:
+        print("Not Weird")
+else:
+    print("Weird")
