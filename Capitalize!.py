de70062246f982b3a456f2a9535dd69423b25699
coder9051 <84278583+coder9051@users.noreply.github.com>
2022-03-06 23:02:30 +0530

    Create Capitalize!.py

---
+++ Capitalize!.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+# Complete the solve function below.
+def solve(s):
+    l = []
+    a = s.split(' ')
+    for i in a:
+        m = i.capitalize()
+        l.append(m)
+    return " ".join(l)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
